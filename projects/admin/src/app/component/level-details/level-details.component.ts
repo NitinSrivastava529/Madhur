28de1a1d116fc0375e7f6664ed196ed933799efa
Nitin Srivastava <63593087+NitinSrivastava529@users.noreply.github.com>
2024-11-30 14:51:28 +0530

    ok

--- projects/admin/src/app/component/level-details/level-details.component.ts
+++ projects/admin/src/app/component/level-details/level-details.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AfterContentChecked, ChangeDetectorRef, Component, inject, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { AfterContentChecked, ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnChanges, OnInit, signal, SimpleChanges, ViewChild } from '@angular/core';
 import { GlobalService } from '../../services/global.service';
 import { FormsModule } from '@angular/forms';
 
@@ -9,27 +9,33 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './level-details.component.html',
-  styleUrl: './level-details.component.css'
+  styleUrl: './level-details.component.css',
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class LevelDetailsComponent implements OnInit {
   levelIncome: any = [];
   levelInfo: any = [];
-  _level:any;
+  _memberId: string = "";
+  IsUpdate = signal<any>("");
+  _level: any;
   IsLoading: boolean = false;
   IsLoadingRewad: boolean = false;
   global = inject(GlobalService);
   _http = inject(HttpClient);
-  reward:any= {
+  reward: any = {
     'AutoId': 0,
     'MemberId': "-",
     'level': '0',
     'Remark': "",
-    'file_path':File
+    'file_path': File
   }
   level: any = [];
-  rewardData:any;
+  rewardData: any;
   ngOnInit(): void {
     this.LevelReport();
+  }
+  setUpdate(AutoId: string) {
+    this.IsUpdate.set(AutoId)
   }
   changeFile(event: any) {
     this.reward.file_path = event.target.files[0];
@@ -38,24 +44,23 @@ export class LevelDetailsComponent implements OnInit {
   SaveReward() {
     console.log(this.reward);
     this.IsLoadingRewad = true;
-    var formData = new FormData();    
+    var formData = new FormData();
     formData.append('AutoId', this.reward.AutoId);
     formData.append('MemberId', this.reward.MemberId);
     formData.append('level', this.reward.level);
     formData.append('Remark', this.reward.Remark);
-    formData.append('file_path',this.reward.file_path);    
+    formData.append('file_path', this.reward.file_path);
 
     this.global.UploadFile('api/Member/AddReward', formData).subscribe((res) => {
-      if(res.result){
+      if (res.result) {
         this.IsLoadingRewad = false;
-        for (var r in this.reward) delete this.reward[r]; 
+        for (var r in this.reward) delete this.reward[r];
       }
-      else
-      {
+      else {
         this.IsLoadingRewad = false;
         alert(res.message)
       }
-              
+
     })
   }
   LevelReport() {
@@ -69,7 +74,7 @@ export class LevelDetailsComponent implements OnInit {
     })
   }
   LevelWiseMember(level: string) {
-    this._level=level;
+    this._level = level;
     this.IsLoading = true;
     this._http.get(this.global.baseUrl + 'api/Member/LevelWiseMember?level=' + level, {}).subscribe({
       next: (data) => {
@@ -81,7 +86,7 @@ export class LevelDetailsComponent implements OnInit {
       }
     })
   }
-  GetReward(memberId:string) {    
+  GetReward(memberId: string) {    
     this.IsLoading = true;
     this._http.get(this.global.baseUrl + 'api/Member/GetReward?MemberId=' + memberId, {}).subscribe({
       next: (data) => {
@@ -93,5 +98,19 @@ export class LevelDetailsComponent implements OnInit {
         console.log(error);
       }
     })
+  }
+  DeleteReward(AutoId: number) {
+    if (confirm('are you sure?')) {   
+      this._http.delete(this.global.baseUrl + 'api/Member/DeleteReward?AutoId=' + AutoId,{headers:this.global.headers}).subscribe((res:any)=>{       
+      alert(res)            
+      })
+    }
+  }
+  EditReward(AutoId: number, Remark: string) {        
+    this._http.put(this.global.baseUrl + 'api/Member/EditReward?AutoId=' + AutoId + '&Remark=' + Remark, {},{headers:this.global.headers}).subscribe((res: any) => {      
+      alert(res)
+      this.IsUpdate.set('');      
+    }
+    )
   }
 }
